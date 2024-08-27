b095f139d8393b5378fd3b016a506571c093d507
yeknam-and-co <valorantmoreyekschool@gmail.com>
2024-08-27 17:03:16 -0400

    giving away api key

---
+++ config.js
@@ -0,0 +1,9 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDXcW18D-jVwbtS_J704YMkJOB3uBInwSw",
+  authDomain: "mrsketial.firebaseapp.com",
+  projectId: "mrsketial",
+  storageBucket: "mrsketial.appspot.com",
+  messagingSenderId: "577200290012",
+  appId: "1:577200290012:web:9f16d6ba91ba5429229c19"
+};
